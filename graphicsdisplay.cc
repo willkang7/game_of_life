6d7aa9ed781c3aca92d7fa14fe8fe6ebc2bc1f81
William Kang <willkang7@gmail.com>
2021-05-15 03:11:40 -0400

    Added optimizations.

--- graphicsdisplay.cc
+++ graphicsdisplay.cc
@@ -7,14 +7,15 @@ GraphicsDisplay::GraphicsDisplay(int windowSize, int gridSize)
 {}
 
 void GraphicsDisplay::notify(std::vector<std::vector<char>> grid) {
+  // Clear screen.
+  w.fillRectangle(0, 0, windowSize, windowSize, Xwindow::White);
+
+  // Draw cells.
   for (int row = 0; row < gridSize; ++row) {
     for (int col = 0; col < gridSize; ++col) {
       if (grid[row][col] == '#') {
         w.fillRectangle(col * cellSize, row * cellSize,
           cellSize - 1, cellSize - 1, Xwindow::Black);
-      } else if (grid[row][col] == '.'){
-        w.fillRectangle(col * cellSize, row * cellSize,
-          cellSize - 1, cellSize - 1, Xwindow::White);
       }
     }
   }
--- main.cc
+++ main.cc
@@ -4,17 +4,16 @@
 #include "graphicsdisplay.h"
 #include "window.h"
 
-
 int main() {
   Grid g;
 
-  g.setup("configurations/glider.txt");
+  g.setup("configurations/gosper_glider_gun.txt");
 
   GraphicsDisplay graphics(500, g.getGrid().size());
 
-  for (int i = 0; i < 100; ++i) {
+  for (int i = 0; i < 1000; ++i) {
     graphics.notify(g.getGrid());
   	g.step();
-    sleep(1);
+  	usleep(200000);
   }
 }
